064fb075bffa43e0380fa4e909ca8c1b4fda76cf
Nate Dunn <hello@natedunn.net>
2025-03-01 16:21:09 -0600

    feat: add a few more info to start page

--- app/start/page.tsx
+++ app/start/page.tsx
@@ -10,7 +10,37 @@ export default async function SetupPage() {
 				<p className='mt-2'>A few things to get started.</p>
 			</div>
 			<div className='prose dark:prose-invert'>
-				<h3>Required</h3>
+				<h3>Whats in the stack?</h3>
+				<ul>
+					<li>Next.js</li>
+					<li>TypeScript</li>
+					<li>Tailwind CSS</li>
+				</ul>
+				<h4>Libraries and integrations</h4>
+				<ul>
+					<li>tRPC — API</li>
+					<li>Better Auth — authentication</li>
+					<li>Drizzle ORM (default driver w/ Neon DB) — Database ORM</li>
+					<li>Nuqs — search params</li>
+					<li>Shadcn ready — components</li>
+					<li>T3 Env — type-safe ENV variables</li>
+				</ul>
+				<hr className='solid'></hr>
+				<h3>Features</h3>
+				<ul>
+					<li>Authentication</li>
+					<li>Type-safe API procedures</li>
+					<li>Basic authorization w/ API middleware</li>
+					<li>Admin roles (w/ authorization)</li>
+					<li>Type-safe search params</li>
+					<li>
+						A few small things that I really hate setting up every time for new projects but feel
+						kind of important to do.
+					</li>
+				</ul>
+				<hr className='solid'></hr>
+				<h3>Instructions</h3>
+				<h4>Required</h4>
 				<ul>
 					<li>
 						Enable auth by adding Github client info to <span className='code'>.env</span> file.
@@ -24,7 +54,7 @@ export default async function SetupPage() {
 						.
 					</li>
 				</ul>
-				<h3>Suggestions</h3>
+				<h4>Suggestions</h4>
 				<ul>
 					<li>
 						Add better-auth plugins before running <span className='code'>pnpm db:auth</span> to
